cb5cc3873716d84816cf77c5ac0b4cc50a3c37c7
Rifa Maritza <rifa.maritza@gmail.com>
2025-04-16 07:52:53 +0800

    Edit map pop up

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 3.5872,
       lng: 125.4987,
       gmaps_url: "https://www.google.com/maps?q=3.5872,125.4987",
-      note: "Kupu-kupu endemik Sulawesi, sering ditemukan di daerah hutan pegunungan."
+      note: "Kupu-kupu endemik Sulawesi, sering ditemukan di daerah hutan pegunungan.",
+      image: "images/habitat1.jpg"
     },
     {
       name: "Tahuna, Sangihe",
@@ -18,7 +19,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 3.6115,
       lng: 125.4683,
       gmaps_url: "https://www.google.com/maps?q=3.6115,125.4683",
-      note: "Spesies besar dengan sayap hitam dan emas, sering dijumpai di hutan sekunder."
+      note: "Spesies besar dengan sayap hitam dan emas, sering dijumpai di hutan sekunder.",
+      image: "images/habitat2.jpeg"
     },
     {
       name: "Pulau Salibabu, Talaud",
@@ -28,7 +30,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 4.2447,
       lng: 126.7893,
       gmaps_url: "https://www.google.com/maps?q=4.2447,126.7893",
-      note: "Sering ditemukan di dekat pantai dan hutan bakau."
+      note: "Sering ditemukan di dekat pantai dan hutan bakau.",
+      image: "images/habitat3.jpg"
     },
     {
       name: "Pulau Karakelang, Talaud",
@@ -38,7 +41,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 4.2632,
       lng: 126.8415,
       gmaps_url: "https://www.google.com/maps?q=4.2632,126.8415",
-      note: "Kupu-kupu ekor panjang yang langka, ditemukan di hutan primer."
+      note: "Kupu-kupu ekor panjang yang langka, ditemukan di hutan primer.",
+      image: "images/habitat4.jpg"
     },
     {
       name: "Beo, Talaud",
@@ -48,7 +52,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 4.0211,
       lng: 126.7044,
       gmaps_url: "https://www.google.com/maps?q=4.0211,126.7044",
-      note: "Spesies berwarna gelap dengan pola unik, sering terlihat di sekitar sungai."
+      note: "Spesies berwarna gelap dengan pola unik, sering terlihat di sekitar sungai.",
+      image: "images/habitat5.jpg"
     },
     {
       name: "Gunung Awu, Siau",
@@ -58,7 +63,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 2.7739,
       lng: 125.3784,
       gmaps_url: "https://www.google.com/maps?q=2.7739,125.3784",
-      note: "Kupu-kupu khas dengan warna putih dan kuning, menghuni hutan pegunungan."
+      note: "Kupu-kupu khas dengan warna putih dan kuning, menghuni hutan pegunungan.",
+      image: "images/habitat6.png"
     },
     {
       name: "Pulau Siau",
@@ -68,7 +74,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 2.7532,
       lng: 125.3675,
       gmaps_url: "https://www.google.com/maps?q=2.7532,125.3675",
-      note: "Jantan memiliki sayap biru metalik, sering dijumpai di kebun dan hutan sekunder."
+      note: "Jantan memiliki sayap biru metalik, sering dijumpai di kebun dan hutan sekunder.",
+      image: "images/habitat7.png"
     },
     {
       name: "Pulau Kabaruan, Talaud",
@@ -78,7 +85,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 4.1204,
       lng: 126.8292,
       gmaps_url: "https://www.google.com/maps?q=4.1204,126.8292",
-      note: "Spesies berwarna oranye terang dengan motif unik, umum di daerah terbuka."
+      note: "Spesies berwarna oranye terang dengan motif unik, umum di daerah terbuka.",
+      image: "images/habitat8.jpeg"
     },
     {
       name: "Tabukan Selatan, Sangihe",
@@ -88,7 +96,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 3.5061,
       lng: 125.4879,
       gmaps_url: "https://www.google.com/maps?q=3.5061,125.4879",
-      note: "Salah satu kupu-kupu terbesar dengan warna emas mencolok, endemik Sulawesi dan Maluku."
+      note: "Salah satu kupu-kupu terbesar dengan warna emas mencolok, endemik Sulawesi dan Maluku.",
+      image: "images/habitat9.png"
     },
     {
       name: "Pulau Nanusa, Talaud",
@@ -98,7 +107,8 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
       lat: 4.4031,
       lng: 127.0648,
       gmaps_url: "https://www.google.com/maps?q=4.4031,127.0648",
-      note: "Migrasi dari wilayah lain, sering ditemukan di lahan terbuka dan kebun."
+      note: "Migrasi dari wilayah lain, sering ditemukan di lahan terbuka dan kebun.",
+      image: "images/habitat10.jpg"
     }
   ];
 
@@ -125,6 +135,7 @@ src="https://unpkg.com/leaflet/dist/leaflet.js"
           Status: ${loc.status}<br>
           Jumlah: ${loc.count}<br>
           <small>${loc.note}</small><br>
+          <img src="${loc.image}" alt="${loc.name}" style="width:100px; height:auto;"><br>
           <a href="${loc.gmaps_url}" target="_blank">Lihat di Google Maps</a>
         `;
         L.marker([loc.lat, loc.lng]).addTo(map).bindPopup(popupContent);
